e34f045a3ab2087cc728140c09fbf28e486d26ee
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-05-27 23:34:41 +0200

    Root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resources :key_results, only: [:create]
   end
   mount RailsEventStore::Browser => '/res' if Rails.env.development?
+  root 'objectives#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
