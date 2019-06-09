c670f3b137323994d7d685236b6abf66b5b9a605
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-06-09 23:29:53 +0100

    Scoring objectives

--- app/read_models/on_key_result_score_updated.rb
+++ app/read_models/on_key_result_score_updated.rb
@@ -3,5 +3,9 @@ class OnKeyResultScoreUpdated
     key_result = KeyResult.find(event.data[:key_result_id])
     key_result.score = event.data[:score] / 100
     key_result.save!
+
+    objective = key_result.objective
+    objective.score = objective.key_results.map(&:score).reduce(:+) / objective.key_results.length
+    objective.save!
   end
 end
